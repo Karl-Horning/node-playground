0eb27de4c15db739a51685a0acc55a35c6d50254
Karl Horning <k.horning@me.com>
2018-10-30 19:31:43 +0000

    Basic files added

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+// index
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+// Run the server
+app.listen(5000, () => console.log('The server is running on port 5000.'));
