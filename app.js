b0115f9838d2815743fb0e469c0f7ecb4c341ac2
Karl Horning <k.horning@me.com>
2018-10-31 23:15:47 +0000

    Graph added and linked graph files

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // index
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+// graph
+app.get('/graph', (req, res) => {
+    res.render('graph');
+});
+
+// 404
+app.get('*', (req, res) => {
+    res.send('404! :(');
 });
 
 // Run the server
