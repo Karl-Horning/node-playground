be9fd282506a49e28204540326ca6eba19794adc
Karl Horning <k.horning@me.com>
2018-11-04 22:32:16 +0000

    Final graph JS added

--- public/resources/js/graph.js
+++ public/resources/js/graph.js
@@ -1,126 +1,194 @@
+// https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+// https://insights.stackoverflow.com/survey/2018/#technology-most-loved-dreaded-and-wanted-languages
 const sample = [{
-        language: "Rust",
+        language: 'Rust',
         value: 78.9,
-        color: "#000000"
+        color: '#000000'
     },
     {
-        language: "Kotlin",
+        language: 'Kotlin',
         value: 75.1,
-        color: "#00a2ee"
+        color: '#00a2ee'
     },
     {
-        language: "Python",
+        language: 'Python',
         value: 68.0,
-        color: "#fbcb39"
+        color: '#fbcb39'
     },
     {
-        language: "TypeScript",
+        language: 'TypeScript',
         value: 67.0,
-        color: "#007bc8"
+        color: '#007bc8'
     },
     {
-        language: "Go",
+        language: 'Go',
         value: 65.6,
-        color: "#65cedb"
+        color: '#65cedb'
     },
     {
-        language: "Swift",
+        language: 'Swift',
         value: 65.1,
-        color: "#ff6e52"
+        color: '#ff6e52'
     },
     {
-        language: "JavaScript",
+        language: 'JavaScript',
         value: 61.9,
-        color: "#f9de3f"
+        color: '#f9de3f'
     },
     {
-        language: "C#",
+        language: 'C#',
         value: 60.4,
-        color: "#5d2f8e"
+        color: '#5d2f8e'
     },
     {
-        language: "F#",
+        language: 'F#',
         value: 59.6,
-        color: "#008fc9"
+        color: '#008fc9'
     },
     {
-        language: "Clojure",
+        language: 'Clojure',
         value: 59.6,
-        color: "#507dca"
+        color: '#507dca'
     }
 ];
 
-const margin = 60;
+const svg = d3.select('svg');
+const svgContainer = d3.select('#container');
+
+const margin = 80;
 const width = 1000 - 2 * margin;
 const height = 600 - 2 * margin;
 
-const svg = d3.select("svg");
-const chart = svg
-    .append("g")
-    .attr("transform", `translate(${margin}, ${margin})`);
+const chart = svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`);
 
-const yScale = d3
-    .scaleLinear()
+const xScale = d3.scaleBand()
+    .range([0, width])
+    .domain(sample.map((s) => s.language))
+    .padding(0.4)
+
+const yScale = d3.scaleLinear()
     .range([height, 0])
     .domain([0, 100]);
 
-chart.append("g").call(d3.axisLeft(yScale));
+// vertical grid lines
+// const makeXLines = () => d3.axisBottom()
+//   .scale(xScale)
 
-const xScale = d3
-    .scaleBand()
-    .range([0, width])
-    .domain(sample.map(s => s.language))
-    .padding(0.2);
+const makeYLines = () => d3.axisLeft()
+    .scale(yScale)
 
-chart
-    .append("g")
-    .attr("transform", `translate(0, ${height})`)
+chart.append('g')
+    .attr('transform', `translate(0, ${height})`)
     .call(d3.axisBottom(xScale));
 
-chart
-    .selectAll()
-    .data(sample)
-    .enter()
-    .append("rect")
-    .attr("x", s => xScale(s.language))
-    .attr("y", s => yScale(s.value))
-    .attr("height", s => height - yScale(s.value))
-    .attr("width", xScale.bandwidth());
+chart.append('g')
+    .call(d3.axisLeft(yScale));
 
-// Vertical grid lines
+// vertical grid lines
 // chart.append('g')
-//     .attr('class', 'grid')
-//     .attr('transform', `translate(0, ${height})`)
-//     .call(d3.axisBottom()
-//         .scale(xScale)
-//         .tickSize(-height, 0, 0)
-//         .tickFormat(''));
+//   .attr('class', 'grid')
+//   .attr('transform', `translate(0, ${height})`)
+//   .call(makeXLines()
+//     .tickSize(-height, 0, 0)
+//     .tickFormat('')
+//   )
 
-// Horizontal grid lines
 chart.append('g')
     .attr('class', 'grid')
-    .call(d3.axisLeft()
-        .scale(yScale)
+    .call(makeYLines()
         .tickSize(-width, 0, 0)
-        .tickFormat(''));
+        .tickFormat('')
+    )
 
-// Labels
-// y axis label
-svg.append('text')
+const barGroups = chart.selectAll()
+    .data(sample)
+    .enter()
+    .append('g')
+
+barGroups
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (g) => xScale(g.language))
+    .attr('y', (g) => yScale(g.value))
+    .attr('height', (g) => height - yScale(g.value))
+    .attr('width', xScale.bandwidth())
+    .on('mouseenter', function (actual, i) {
+        d3.selectAll('.value')
+            .attr('opacity', 0)
+
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 0.6)
+            .attr('x', (a) => xScale(a.language) - 5)
+            .attr('width', xScale.bandwidth() + 10)
+
+        const y = yScale(actual.value)
+
+        line = chart.append('line')
+            .attr('id', 'limit')
+            .attr('x1', 0)
+            .attr('y1', y)
+            .attr('x2', width)
+            .attr('y2', y)
+
+        barGroups.append('text')
+            .attr('class', 'divergence')
+            .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+            .attr('y', (a) => yScale(a.value) + 30)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .text((a, idx) => {
+                const divergence = (a.value - actual.value).toFixed(1)
+
+                let text = ''
+                if (divergence > 0) text += '+'
+                text += `${divergence}%`
+
+                return idx !== i ? text : '';
+            })
+
+    })
+    .on('mouseleave', function () {
+        d3.selectAll('.value')
+            .attr('opacity', 1)
+
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 1)
+            .attr('x', (a) => xScale(a.language))
+            .attr('width', xScale.bandwidth())
+
+        chart.selectAll('#limit').remove()
+        chart.selectAll('.divergence').remove()
+    })
+
+barGroups
+    .append('text')
+    .attr('class', 'value')
+    .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+    .attr('y', (a) => yScale(a.value) + 30)
+    .attr('text-anchor', 'middle')
+    .text((a) => `${a.value}%`)
+
+svg
+    .append('text')
+    .attr('class', 'label')
     .attr('x', -(height / 2) - margin)
     .attr('y', margin / 2.4)
     .attr('transform', 'rotate(-90)')
     .attr('text-anchor', 'middle')
-    .text('Love meter (%)');
+    .text('Love meter (%)')
 
-// x axis label
 svg.append('text')
-    .attr('x', (width / 2) + margin)
-    .attr('y', height + (margin * 2 - 17))
+    .attr('class', 'label')
+    .attr('x', width / 2 + margin)
+    .attr('y', height + margin * 1.7)
     .attr('text-anchor', 'middle')
-    .text('Languages');
+    .text('Languages')
 
-// Graph title
 svg.append('text')
     .attr('class', 'title')
     .attr('x', width / 2 + margin)
@@ -128,7 +196,6 @@ svg.append('text')
     .attr('text-anchor', 'middle')
     .text('Most loved programming languages in 2018')
 
-// Graph attribution
 svg.append('text')
     .attr('class', 'source')
     .attr('x', width - margin / 2)
