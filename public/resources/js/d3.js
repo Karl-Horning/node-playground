916309d48647e084fb1676da8745c49810e63684
Karl Horning <k.horning@me.com>
2018-11-06 16:51:54 +0000

    Final freeCodeCamp exercises added

--- public/resources/js/d3.js
+++ public/resources/js/d3.js
@@ -407,6 +407,240 @@
 
 
 // 21 Data Visualization with D3: Create a Scatterplot with SVG Circles
+// const dataset = [
+//     [34, 78],
+//     [109, 280],
+//     [310, 120],
+//     [79, 411],
+//     [420, 220],
+//     [233, 145],
+//     [333, 96],
+//     [222, 333],
+//     [78, 320],
+//     [21, 123]
+// ];
+
+// const w = 500;
+// const h = 500;
+
+// svg = d3
+//     .select('body')
+//     .append('svg')
+//     .attr('width', w)
+//     .attr('height', h);
+
+// svg
+//     .selectAll('circle')
+//     .data(dataset)
+//     .enter()
+//     .append('circle');
+
+
+// 22 Data Visualization with D3: Add Attributes to the Circle Elements
+// const dataset = [
+//     [34, 78],
+//     [109, 280],
+//     [310, 120],
+//     [79, 411],
+//     [420, 220],
+//     [233, 145],
+//     [333, 96],
+//     [222, 333],
+//     [78, 320],
+//     [21, 123]
+// ];
+
+// const w = 500;
+// const h = 500;
+
+// svg = d3
+//     .select('body')
+//     .append('svg')
+//     .attr('width', w)
+//     .attr('height', h);
+
+// svg
+//     .selectAll('circle')
+//     .data(dataset)
+//     .enter()
+//     .append('circle')
+//     .attr('cx', d => d[0])
+//     .attr('cy', d => h - d[1])
+//     .attr('r', 5);
+
+
+// 23 Data Visualization with D3: Add Labels to Scatter Plot Circles
+// const dataset = [
+//     [34, 78],
+//     [109, 280],
+//     [310, 120],
+//     [79, 411],
+//     [420, 220],
+//     [233, 145],
+//     [333, 96],
+//     [222, 333],
+//     [78, 320],
+//     [21, 123]
+// ];
+
+// const w = 500;
+// const h = 500;
+
+// svg = d3
+//     .select('body')
+//     .append('svg')
+//     .attr('width', w)
+//     .attr('height', h);
+
+// svg
+//     .selectAll('circle')
+//     .data(dataset)
+//     .enter()
+//     .append('circle')
+//     .attr('cx', d => d[0])
+//     .attr('cy', d => h - d[1])
+//     .attr('r', 5);
+
+// svg
+//     .selectAll('text')
+//     .data(dataset)
+//     .enter()
+//     .append('text')
+//     .text(d => `${d[0]}, ${d[1]}`)
+//     .attr('x', d => d[0] + 5)
+//     .attr('y', d => h - d[1]);
+
+
+// 24 Data Visualization with D3: Create a Linear Scale with D3
+// const scale = d3.scaleLinear();
+// const output = scale(50);
+
+// d3
+//     .select('body')
+//     .append('h2')
+//     .text(output);
+
+
+// 25 Data Visualization with D3: Set a Domain and a Range on a Scale
+// const scale = d3
+//     .scaleLinear()
+//     .domain([250, 500])
+//     .range([10, 150]);
+// const output = scale(50);
+
+// d3
+//     .select('body')
+//     .append('h2')
+//     .text(output);
+
+
+// 26 Data Visualization with D3: Use the d3.max and d3.min Functions to 
+// Find Minimum and Maximum Values in a Dataset
+// const positionData = [
+//     [1, 7, -4],
+//     [6, 3, 8],
+//     [2, 8, 3]
+// ];
+// const output = d3.max(positionData, (d) => d[2]);
+
+// d3
+//     .select('body')
+//     .append('h2')
+//     .text(output);
+
+
+// 27 Data Visualization with D3: Use Dynamic Scales
+// const dataset = [
+//     [34, 78],
+//     [109, 280],
+//     [310, 120],
+//     [79, 411],
+//     [420, 220],
+//     [233, 145],
+//     [333, 96],
+//     [222, 333],
+//     [78, 320],
+//     [21, 123]
+// ];
+
+// const w = 500;
+// const h = 500;
+
+// // Padding between the SVG canvas boundary and the plot
+// const padding = 30;
+
+// // Create an x and y scale
+// const xScale = d3
+//     .scaleLinear()
+//     .domain([0, d3.max(dataset, (d) => d[0])])
+//     .range([padding, w - padding]);
+
+// const yScale = d3
+//     .scaleLinear()
+//     .domain([0, d3.max(dataset, (d) => d[1])])
+//     .range([h - padding, padding]);
+
+// const output = yScale(411); // Returns 30
+// d3
+//     .select("body")
+//     .append("h2")
+//     .text(output)
+
+
+// 28 Data Visualization with D3: Use a Pre-Defined Scale to Place Elements
+// const dataset = [
+//     [34, 78],
+//     [109, 280],
+//     [310, 120],
+//     [79, 411],
+//     [420, 220],
+//     [233, 145],
+//     [333, 96],
+//     [222, 333],
+//     [78, 320],
+//     [21, 123]
+// ];
+
+// const w = 500;
+// const h = 500;
+// const padding = 60;
+
+// const xScale = d3
+//     .scaleLinear()
+//     .domain([0, d3.max(dataset, (d) => d[0])])
+//     .range([padding, w - padding]);
+
+// const yScale = d3
+//     .scaleLinear()
+//     .domain([0, d3.max(dataset, (d) => d[1])])
+//     .range([h - padding, padding]);
+
+// const svg = d3
+//     .select('body')
+//     .append('svg')
+//     .attr('width', w)
+//     .attr('height', h);
+
+// svg
+//     .selectAll('circle')
+//     .data(dataset)
+//     .enter()
+//     .append('circle')
+//     .attr('cx', d => xScale(d[0]))
+//     .attr('cy', d => yScale(d[1]))
+//     .attr('r', 5);
+
+// svg
+//     .selectAll('text')
+//     .data(dataset)
+//     .enter()
+//     .append('text')
+//     .text((d) => (`${d[0]}, ${d[1]}`))
+//     .attr('x', d => xScale(d[0] + 10))
+//     .attr('y', d => yScale(d[1]));
+
+
+// 29 Data Visualization with D3: Add Axes to a Visualization
 const dataset = [
     [34, 78],
     [109, 280],
@@ -422,12 +656,54 @@ const dataset = [
 
 const w = 500;
 const h = 500;
+const padding = 60;
 
-svg = d3
+const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[0])])
+    .range([padding, w - padding]);
+
+const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h - padding, padding]);
+
+const svg = d3
     .select('body')
     .append('svg')
     .attr('width', w)
-    .attr('height', h)
+    .attr('height', h);
+
+svg
+    .selectAll('circle')
+    .data(dataset)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', 5);
+
+svg
+    .selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .text((d) => (`${d[0]}, ${d[1]}`))
+    .attr('x', d => xScale(d[0] + 10))
+    .attr('y', d => yScale(d[1]));
+
+const xAxis = d3.axisBottom(xScale);
+// Add your code below this line
+const yAxis = d3.axisLeft(yScale);
+// Add your code above this line
+
+svg.append("g")
+    .attr("transform", `translate(0, ${(h - padding)}`)
+    .call(xAxis);
+
+svg.append("g")
+    .attr("transform", `translate(${(h - padding), 0}`)
+    .call(yAxis);
 
 
 // Test data
